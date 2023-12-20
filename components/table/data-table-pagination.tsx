bed5d00f3c43f1edb9208c6ff9ae7aab86873e4c
Dharanitharan <dharanitharanp906@gmail.com>
2023-12-20 20:20:00 +0000

    commit

--- components/table/data-table-pagination.tsx
+++ components/table/data-table-pagination.tsx
@@ -23,7 +23,7 @@ export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex items-center justify-between px-2">
+    <div className="flex items-center justify-between px-2 mt-5">
       <div className="flex-1 text-sm text-muted-foreground">
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
